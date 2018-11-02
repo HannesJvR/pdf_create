3d45c01b1e78d613da1efcdf55cdd5c8ef663599
HannesJVR <hannes.jansenvanrensburg@za.ey.com>
2018-11-02 08:39:49 +0200

    Add new class to store the attributes required to build a SubsidyLetter

---
+++ src/main/java/com/ey/za/pdf_create/SubsidyLetter.java
@@ -0,0 +1,38 @@
+package com.ey.za.pdf_create;
+
+public class SubsidyLetter {
+	String LoanApplicationNumber = "";
+	String PayrollCode = "";
+	String RecID = "";
+	String OctPayment = "";
+	String CorrectAmount = "";
+	String SepApps = "";
+	String Variance = "";
+	String NovRecon = "";
+	String Recon = "";
+	String employeeno = "";
+	String party_id = "";
+	String FirstNames = "";
+	String Surname = "";
+	String email = "";
+	String filepath = "";
+
+	SubsidyLetter() {
+		LoanApplicationNumber = "";
+		PayrollCode = "";
+		RecID = "";
+		OctPayment = "";
+		CorrectAmount = "";
+		SepApps = "";
+		Variance = "";
+		NovRecon = "";
+		Recon = "";
+		employeeno = "";
+		party_id = "";
+		FirstNames = "";
+		Surname = "";
+		email = "";
+		filepath = "";
+	}
+
+}
